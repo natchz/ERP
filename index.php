e038fed6e5fd1c1e12885e3162a88992d3aec1a4
Luis García <luis.garciap@upb.edu.co>
2017-08-17 08:54:53 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Welcome to AMOVIL Nginx Server</title>
+</head>
+<body>
+	<center>
+		<h1>Welcome to AMOVIL Nginx Server</h1>
+		<h2>Everithing is working fine.</h2>
+	</center>
+	<?php
+		phpinfo();
+	?>
+</body>
+</html>
